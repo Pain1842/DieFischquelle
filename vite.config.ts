93e3a1e7fa61d224a7055407dbc6cc2477d13491
Taner <taner.aksoy@student.hs-rm.de>
2024-10-14 14:30:04 +0200

    fix: repo name

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/REPOSITORY_NAME/',  // <--- Füge das hier hinzu
+  base: '/DieFischquelle/',
   plugins: [react()],
   css: {
     preprocessorOptions: {
